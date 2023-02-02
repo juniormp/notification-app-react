a1627fa69145bedec34625e04e68ffafd82c12f7
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-02-02 20:09:57 -0300

    add css

--- src/components/LogHistory.js
+++ src/components/LogHistory.js
@@ -1,14 +1,24 @@
 function LogHistory({log}) {
   return (
-    <ul>
-      {log?.map((log) => (
-        <li key={Math.random()}>
-          <strong>Category:</strong> {log.category}
-          <br />
-          <strong>Message:</strong> {log.content}
-        </li>
-      ))}
-    </ul>
+    <section class="hero is-primary is-fullheight">
+      <div class="hero-body">
+        <div class="content">
+          {log?.map((log) => (
+            <div class="columns is-gapless">
+              <div class="card">
+                <div class="card-content is-large">
+                  <label class="label">
+                    <strong>{log.category}</strong>
+                    <br />
+                    <strong>{log.content}</strong>
+                  </label>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/components/NotificationForm.js
+++ src/components/NotificationForm.js
@@ -26,40 +26,62 @@ function NotificatioForm() {
   };
 
   return (
-    <div>
-      <form>
-        <label>
-          Category:
-          <select
-            value={category}
-            onChange={(event) => setCategory(event.target.value)}
-          >
-            <option hidden value="">
-              Select Category
-            </option>
-            {categories.map((cat) => (
-              <option key={cat} value={cat}>
-                {cat}
-              </option>
-            ))}
-          </select>
-        </label>
-        <br />
-        <label>
-          Message:
-          <textarea
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-          />
-        </label>
-        <br />
-        <button onClick={handleSubmit} type="button">
-          Submit
-        </button>
-      </form>
-      <hr />
+    <section class="hero is-primary is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-5-tablet is-4-desktop is-3-widescreen">
+            <div class="column">
+              <form class="box">
+                <div class="field has-text-centered">
+                  <label class="label">Notification App</label>
+                </div>
+                <div class="field">
+                  <label class="label">Category</label>
+                  <div class="select is-primary">
+                    <select
+                      value={category}
+                      onChange={(event) => setCategory(event.target.value)}
+                    >
+                      <option hidden value="">
+                        Select Category
+                      </option>
+                      {categories.map((cat) => (
+                        <option key={cat} value={cat}>
+                          {cat}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <br />
+                  <br />
+                  <div class="field">
+                    <label class="label">
+                      Message
+                      <textarea
+                        class="textarea"
+                        placeholder="Describe your message"
+                        value={message}
+                        onChange={(event) => setMessage(event.target.value)}
+                      />
+                    </label>
+                  </div>
+                  <div class="field">
+                    <button
+                      class="button is-success"
+                      onClick={handleSubmit}
+                      type="button"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
       <LogHistory log={log} />
-    </div>
+    </section>
   );
 }
 
